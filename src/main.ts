e42b0e6d448998fac971ad4ea379f109b08bbb40
jadu <jeff.test@testing.com>
2025-07-02 19:17:15 +0100

    amplify add and default landing page created

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import 'zone.js';  
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports); // 🔹 Configure Amplify here
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
